ec44c238cdafc84595a0d46e00d33b84b8761e7f
3dorange <3dorange@gmail.com>
2014-12-11 00:14:48 +0300

    Minor Update
    Made first level

---
+++ Assets/Scripts/GameLogic/BaseAliveObject.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseAliveObject : MonoBehaviour 
+{
+	//Базовый класс для всех живых организмов в подземелье
+
+	private float CurrentHealth;			//текущее значение здоровья
+	public float MaxHealth;					//стартовое значение здоровья
+
+	private float CurrentSpeed;				//текущая скорость перемещения
+	public float MaxSpeed;					//максимальная скорость
+
+	private bool Grounded;					//Касается земли или нет
+
+	void Start()
+	{
+		CurrentSpeed = 0;
+		CurrentHealth = MaxHealth;
+		Grounded = false;
+	}
+
+	void Update()
+	{
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		//Проверяем на попадение в триггеры
+		switch (other.tag)
+		{
+			case "MakeDecisionZone":
+				ChooseWay(other.GetComponent<MakeDecisionZone>());
+			break;
+
+			default:
+				break;
+		}
+	}
+
+	private void ChooseWay(MakeDecisionZone decisionZone)
+	{
+		//Выбираем направление движения
+	}
+}
---
+++ Assets/Scripts/GameLogic/MakeDecisionZone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeDecisionZone : MonoBehaviour 
+{
+	//Возможные направления движения
+	public enum MoveDirections
+	{
+		Right,
+		Left,
+		Up,
+		Down
+	}
+
+	public MoveDirections[] moveDirectionForThisPoint;				//Возможные варианты направления движения в данной точке
+
+}
